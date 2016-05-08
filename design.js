cb8dfb2cf25e1f5adc81a8a8f121e54280235b98
polar2031 <polar2031@gmail.com>
2016-05-07 22:14:54 -0400

    5/7

--- design.js
+++ design.js
@@ -37,5 +37,6 @@ function processData(errors, map){
 
 // Use the queue library to load multiple files and then process them
 queue()
-  .defer(d3.json, "鄰界圖.geojson")
+  // .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/%E9%84%B0%E7%95%8C%E5%9C%96.geojson")
+  .defer(d3.json, "taipei.geojson")
   .await(processData);
