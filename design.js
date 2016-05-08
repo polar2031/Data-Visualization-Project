b85a640ad59d6ce961ff87c1f99b0465476ef8b0
polar2031 <polar2031@gmail.com>
2016-05-08 18:58:40 -0400

    data update

--- design.js
+++ design.js
@@ -1,42 +1,109 @@
- function visitsChoropleth(divId, map){
-  //set canvus size
-  var width = 600;
-  var height = 600;
+var width = 1000;
+var height = 800;
+var projection = d3.geo.equirectangular()
+  .center([121.58,25.09])
+  .scale(170000)
+  .translate([width / 2, height / 2]);
 
-  var svg = d3.select(divId)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+function initialMap(divId){
+    var svg = d3.select(divId)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+}
+ // #     #    #    ######
+ // ##   ##   # #   #     #
+ // # # # #  #   #  #     #
+ // #  #  # #     # ######
+ // #     # ####### #
+ // #     # #     # #
+ // #     # #     # #
 
-  //set projection function
-  var projection = d3.geo.equirectangular()
-    .center([121.58,25.07])
-    .scale(120000)
-    .translate([width / 2, height / 2]);
-  //set projection function for path
-  var path = d3.geo.path().projection(projection);
+function drawMap(divId, mapData){
+    var svg = d3.select(divId)
+        .select("svg")
+        .append("g")
+        .attr("class", "map")
+    var path = d3.geo.path().projection(projection);
+    var color = d3.scale.category20();
+    var states = svg.selectAll("path")
+        .data(mapData.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "district")
+        .style("fill", function(d, i) {
+            return "rgb(154, 179, 202)";
+        });
+}
+ // ######  ####### #     # ####### #######  #####
+ // #     # #     # #     #    #    #       #     #
+ // #     # #     # #     #    #    #       #
+ // ######  #     # #     #    #    #####    #####
+ // #   #   #     # #     #    #    #             #
+ // #    #  #     # #     #    #    #       #     #
+ // #     # #######  #####     #    #######  #####
+function drawRoutes(divId, routesData){
+    var svg = d3.select(divId)
+        .select("svg")
+        .append("g")
+        .attr("class", "routes")
+    var path = d3.geo.path().projection(projection);
+    console.log(routesData);
+    var routes = svg.selectAll("path")
+        .data(routesData.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("class", "route")
+}
+ //  #####  #######    #    ####### ### ####### #     #  #####
+ // #     #    #      # #      #     #  #     # ##    # #     #
+ // #          #     #   #     #     #  #     # # #   # #
+ //  #####     #    #     #    #     #  #     # #  #  #  #####
+ //       #    #    #######    #     #  #     # #   # #       #
+ // #     #    #    #     #    #     #  #     # #    ## #     #
+ //  #####     #    #     #    #    ### ####### #     #  #####
 
-  var color = d3.scale.category20();
+function drawStations(divId, stationsData){
+    var svg = d3.select(divId)
+         .select("svg")
+         .append("g")
+         .attr("class", "stations");
 
-  var states = svg.append("g")
-    .selectAll("path")
-    .data(map.features)
-    .enter()
-    .append("path")
-    .attr("d", path);
-
-  states.attr("class", "state-boundary")
-    .style("fill", function(d, i) {
-      return "none"
-      //"rgb(174, 199, 232)";
-    });
+    var routes = svg.selectAll("circle")
+         .data(stationsData.features)
+         .enter()
+         .append("circle")
+         .attr("cx", function(d){
+             return projection(d.geometry.coordinates)[0];
+         })
+         .attr("cy", function(d){
+             return projection(d.geometry.coordinates)[1];
+         })
+         .attr("r", 3)
+         .attr("class", "station");
 }
 
-function processData(errors, map){
-  visitsChoropleth("#usage", map);
+function stationsWithIncome(divId, enter, leave){
+    console.log(enter);
+    console.log(leave);
+}
+
+function processData(errors, mapData, routesData, stationsData, enter, leave, rainfall){
+    initialMap("#canvas");
+    drawMap("#canvas", mapData);
+    drawRoutes("#canvas", routesData);
+    drawStations("#canvas", stationsData);
+    stationsWithIncome("#canvas", enter, leave);
 }
 
 // Use the queue library to load multiple files and then process them
 queue()
-  .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/routes.geojson")
-  .await(processData);
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/taipei_district.geojson")
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/routes.geojson")
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/stations.geojson")
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/in_11_2015.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/out_11_2015.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/polar2031/Data-Visualization-Project/master/rainfall2015.json")
+    .await(processData);
