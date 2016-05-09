bf1a7bbf481622ed5aaa4bb66e6f8286ba61b8e9
polar2031 <polar2031@gmail.com>
2016-05-08 20:57:57 -0400

    data update

--- design.js
+++ design.js
@@ -85,9 +85,40 @@ function drawStations(divId, stationsData){
          .attr("class", "station");
 }
 
-function stationsWithIncome(divId, enter, leave){
+function stationsWithUsage(divId, enter, leave){
+    var allStations = Object.keys(enter[0]).slice(1);
+    console.log(allStations)
     console.log(enter);
-    console.log(leave);
+
+    var totalUsage = d3.map();
+    allStations.forEach(function(d){
+        totalUsage.set(d + "站", 0);
+    })
+    enter.forEach(function(d){
+        allStations.forEach(function(d1){
+            totalUsage.set(d1 + "站", totalUsage.get(d1 + "站") + parseInt(d[d1].replace(/\,/g,"")));
+        })
+    })
+    leave.forEach(function(d){
+        allStations.forEach(function(d1){
+            totalUsage.set(d1 + "站", totalUsage.get(d1 + "站") + parseInt(d[d1].replace(/\,/g,"")));
+        })
+    })
+    console.log(totalUsage);
+
+    var stations = d3.select(divId)
+        .select("svg")
+        .select(".stations")
+        .selectAll("circle")
+        .attr("r", function(d){
+            if(totalUsage.has(d.properties.NAME)){
+                return totalUsage.get(d.properties.NAME)/100000;
+            }
+            else{
+                console.log(d.properties.NAME);
+                return 0;
+            }
+        });
 }
 
 function processData(errors, mapData, routesData, stationsData, enter, leave, rainfall){
@@ -95,7 +126,7 @@ function processData(errors, mapData, routesData, stationsData, enter, leave, ra
     drawMap("#canvas", mapData);
     drawRoutes("#canvas", routesData);
     drawStations("#canvas", stationsData);
-    stationsWithIncome("#canvas", enter, leave);
+    stationsWithUsage("#canvas", enter, leave);
 }
 
 // Use the queue library to load multiple files and then process them
